db45248d76c8411dc2ee749b3eadad22e0dd3674
JoelDev13 <joel13r11@gmail.com>
2024-11-27 18:24:50 -0400

    add files

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -3,5 +3,6 @@ import './style.css'
 //import './topics/02-objecto-interfaces';
 //import './topics/03-funciones';
 //import './topics/04-tarea-types'
-import './topics/05-basic-desestructuracion'
+//import './topics/05-basic-desestructuracion'
+import './topics/06-desestructuracion-de-funciones'
 
